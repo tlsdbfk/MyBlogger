c835f80f5c3b1401ea43c92c5a557ca5922cb146
ddubii <wow_boo@sookmyung.ac.kr>
2022-07-26 00:22:10 +0900

    Update RetrofitAPI.kt

--- app/src/main/java/com/hb/myblogger/RetrofitAPI.kt
+++ app/src/main/java/com/hb/myblogger/RetrofitAPI.kt
@@ -1,10 +1,15 @@
 package com.hb.myblogger
 
 import com.google.gson.JsonObject
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface RetrofitAPI {
     @GET("/todos")//서버에 GET요청을 할 주소를 입력
     fun getTodoList() : Call<JsonObject> //MainActivity에서 사용할 json파일 가져오는 메서드
+
 }
